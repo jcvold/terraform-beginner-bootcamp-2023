e791b32feca7499dd7e05035f6e04410b97798bb
John Vold <jcvold@gmail.com>
2023-10-04 14:54:32 +0000

    38-implemet provider block in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for static website"
-  value       = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for static website"
+#   value       = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value       = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value       = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_endpoint" {
-  description = "CDN static website hosting endpoint"
-  value       = module.terrahouse_aws.cloudfront_endpoint
-}
+# output "cloudfront_endpoint" {
+#   description = "CDN static website hosting endpoint"
+#   value       = module.terrahouse_aws.cloudfront_endpoint
+# }

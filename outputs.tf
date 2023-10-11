9ff19691fe69a0eb165d7df230366be588cc0665
John Vold <78934911+jcvold@users.noreply.github.com>
2023-10-11 14:48:00 -0500

    45-refactored resources, switched town deployment (#46)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for static website"
-  value       = module.terrahouse_aws.bucket_name
+  value       = module.home_boardgame_hosting
 }
 
 output "s3_website_endpoint" {
   description = "S3 static website hosting endpoint"
-  value       = module.terrahouse_aws.website_endpoint
+  value       = module.home_boardgame_hosting.website_endpoint
 }
 
 output "cloudfront_url" {
   description = "CDN static website hosting endpoint"
-  value       = module.terrahouse_aws.cloudfront_url
+  value       = module.home_boardgame_hosting.domain_name
 }
